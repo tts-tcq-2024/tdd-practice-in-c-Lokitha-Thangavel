1e89c139bb17fc75e44aa9dc618e0be9cb060718
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-08 10:50:57 +0530

    Refactored code

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,8 +1,17 @@
-int add (const char* str)
+boolean isEmptyString(const char* input)
 {
   if (str == NULL || str[0] == '\0') 
   {
-     return 0; // The string is empty
+     return TRUE; // The string is empty
+  }
+  return FALSE;
+}
+
+int add (const char* str)
+{
+  if(isEmptyString(str) == TRUE)
+  {
+     return 0;
   }
   return -1;
 }
