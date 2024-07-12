1763620db27e8704aa9de36b88a0cf9040cdb788
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-12 10:29:57 +0530

    Single string case update

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,3 +1,12 @@
+int isSingleInput(const char* input)
+{
+  if (input[1] == '\0') 
+  {
+     return 1; // The string is empty
+  }
+  return 0;
+}
+
 int isEmptyString(const char* input)
 {
   if (input == NULL || input[0] == '\0') 
@@ -12,6 +21,10 @@ int add (const char* str)
   if(isEmptyString(str))
   {
      return 0;
+  }
+  else if(isSingleInput(str))
+  {
+     return int(str);
   }
   return -1;
 }
