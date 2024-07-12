a5d7f141b52c118780f3e76501ebc121164d9509
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-12 15:44:41 +0530

    Added function for custom delimiters

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,19 +9,55 @@ int isNumGreaterThanThousand(int num)
     }
     return 0;
 }
+
+const char* extractDelimiters(const char* numbers, const char** delimiters) 
+{    
+    const char* numberStart = numbers;
+
+    if (numbers[0] == '/' && numbers[1] == '/') 
+    {
+        const char* delimiterEnd = strstr(numbers, "\n");
+        if (delimiterEnd != NULL) 
+        {
+            numberStart = delimiterEnd + 1;
+            size_t delimiterLength;
+            if (numbers[2] == '[') 
+            {
+                delimiterLength = delimiterEnd - numbers - 3;
+                char* customDelimiter = (char*)malloc(delimiterLength);
+                strncpy(customDelimiter, numbers + 3, delimiterLength);
+                customDelimiter[delimiterLength - 1] = '\0';
+                *delimiters = customDelimiter;
+            } 
+            else 
+            {
+                delimiterLength = delimiterEnd - numbers - 2;
+                char* customDelimiter = (char*)malloc(delimiterLength + 1);
+                strncpy(customDelimiter, numbers + 2, delimiterLength);
+                customDelimiter[delimiterLength] = '\0';
+                *delimiters = customDelimiter;
+            }
+        }
+    }
+    return numberStart;
+}
+
 int tokenizeString(const char* number)
 {
     char* inputCopy = strdup(number);
     const char* delimiters = ",\n";
+    const char* numberStart = extractDelimiters(number, &delimiters);
     char* token = strtok(inputCopy, delimiters);
     int sum = 0;
     int num = 0;
 
+    
+
     while (token != NULL) {
         num = atoi(token);
-        if(!isNumGreaterThanThousand(num))
+        if(!isNumGreaterThanThousand(numberStart))
         {
-            sum += num;
+            sum += numberStart;
         } 
         token = strtok(NULL, delimiters);
     }
